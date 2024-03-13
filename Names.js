212a31ec7174a39eab0e03d7ef5719d8b24505a6
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:24:32 +0500

    first commit

---
+++ Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Hamza Nasir', 'Mubashir', 'Anus', 'Farsat'];
+const message = 'Welcome to typescript class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,12 @@
+
+let names :string[] = ['Hamza Nasir','Mubashir','Anus','Farsat'] ;
+
+const message :string  = 'Welcome to typescript class: '
+
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+    
+}
+
+
